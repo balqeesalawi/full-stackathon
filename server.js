1f74487c8781bc8df965c932c7ba6d68835fd022
envwx <naz66cod@gmail.com>
2025-10-21 07:50:12 +0300

    added Player controller and router

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-const express = require("express")
-const logger = require("morgan")
+const express = require('express')
+const logger = require('morgan')
+const gameRouter = require('./routes/gameRouter')
 
 const PORT = process.env.PORT || 3000
 
-const db = require("./db")
+const db = require('./db')
 
 const app = express()
 
-app.use(logger("dev"))
+app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use('/games', gameRouter)
 
-app.use("/", (req, res) => {
+app.use('/', (req, res) => {
   res.send(`Connected!`)
 })
+
+app.use(express.urlencoded({ extended: false }))
 
 app.listen(PORT, () => {
   console.log(`Running Express server on Port ${PORT} . . .`)
